24e6665dac6efc6001528faa363dcdb1703fecb8
Sangeet Kumar <sk@urantiatech.com>
2019-11-10 22:21:25 +0530

    Renaming of structs

--- levenshtein.go
+++ levenshtein.go
@@ -3,9 +3,9 @@ package levenshtein
 // The Trie data structure keeps a set of words, organized with one node for
 // each letter. Each node has a branch for each letter that may follow it in the
 // set of words.
-type TrieNode struct {
+type Levenshtein struct {
 	Word     string
-	Children map[string]*TrieNode
+	Children map[string]*Levenshtein
 }
 
 type Result struct {
@@ -13,18 +13,16 @@ type Result struct {
 	Cost int
 }
 
-func New() *TrieNode {
-	node := &TrieNode{}
-	node.Children = make(map[string]*TrieNode)
+func New() *Levenshtein {
+	node := &Levenshtein{}
+	node.Children = make(map[string]*Levenshtein)
 	return node
 }
 
-func (t *TrieNode) Insert(word string) {
-	// fmt.Println("Insert:", word)
-	node := t
+func (l *Levenshtein) Insert(word string) {
+	node := l
 	for _, letter := range []rune(word) {
 		k := string(letter)
-		// fmt.Println("Insert k:", k)
 		if _, ok := node.Children[k]; !ok {
 			node.Children[k] = New()
 		}
@@ -35,12 +33,12 @@ func (t *TrieNode) Insert(word string) {
 
 // The search function returns a list of all words that are less than the given
 // maximum distance from the target word
-func (t *TrieNode) Search(word string, maxCost int) []Result {
+func (l *Levenshtein) Search(word string, maxCost int) []Result {
 	// build first row
-	currentRow := intRange(len(word) + 1)
+	currentRow := initRange(len(word) + 1)
 	results := []Result{}
 
-	for k, v := range t.Children {
+	for k, v := range l.Children {
 		r := v.searchr(k, word, currentRow, maxCost)
 		for _, result := range r {
 			results = append(results, result)
@@ -51,9 +49,9 @@ func (t *TrieNode) Search(word string, maxCost int) []Result {
 
 // This recursive helper is used by the search function above. It assumes that
 // the previousRow has been filled in already.
-func (t *TrieNode) searchr(key, word string, previousRow []int, maxCost int) []Result {
+func (l *Levenshtein) searchr(key, word string, previousRow []int, maxCost int) []Result {
 	columns := len(word) + 1
-	currentRow := intRange(previousRow[0] + 1)
+	currentRow := initRange(previousRow[0] + 1)
 	results := []Result{}
 
 	// Build one row for the letter, with a column for each letter in the target
@@ -62,8 +60,6 @@ func (t *TrieNode) searchr(key, word string, previousRow []int, maxCost int) []R
 		insertCost := currentRow[column-1] + 1
 		deleteCost := previousRow[column] + 1
 		replaceCost := 0
-
-		// fmt.Println("Costs:", insertCost, deleteCost)
 
 		letters := []rune(word)
 		if string(letters[column-1]) != key {
@@ -77,15 +73,15 @@ func (t *TrieNode) searchr(key, word string, previousRow []int, maxCost int) []R
 
 	// if the last entry in the row indicates the optimal cost is less than the
 	// maximum cost, and there is a word in this trie node, then add it.
-	if currentRow[len(currentRow)-1] <= maxCost && t.Word != "" {
-		r := Result{Word: t.Word, Cost: currentRow[len(currentRow)-1]}
+	if currentRow[len(currentRow)-1] <= maxCost && l.Word != "" {
+		r := Result{Word: l.Word, Cost: currentRow[len(currentRow)-1]}
 		results = append(results, r)
 	}
 
 	// if any entries in the row are less than the maximum cost, then
 	// recursively search each branch of the trie
 	if minCost(currentRow) <= maxCost {
-		for k, v := range t.Children {
+		for k, v := range l.Children {
 			r := v.searchr(k, word, currentRow, maxCost)
 			for _, result := range r {
 				results = append(results, result)
@@ -95,7 +91,7 @@ func (t *TrieNode) searchr(key, word string, previousRow []int, maxCost int) []R
 	return results
 }
 
-func intRange(size int) []int {
+func initRange(size int) []int {
 	var list []int
 	for i := 0; i < size; i++ {
 		list = append(list, i)
